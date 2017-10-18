1abaae583f3bb7c3bc9c44be76d5d0eb873fe5a4
sbglive <benoit@sbg-live.com>
2017-10-18 13:32:24 +0800

    update with cache

--- OdooApi.js
+++ OdooApi.js
@@ -1,6 +1,9 @@
 const Logger = require('xvi-logger');
 const Helper = require('xvi-helper');
 const OdooCoreApi = require('xvi-odooapi-core');
+const readDirFiles = require('read-dir-files');
+const moment = require('moment-timezone');
+const del = require('del');
 
 class OdooApi {
     constructor({
@@ -17,6 +20,11 @@ class OdooApi {
         maxRecordsPerCall = 200,
         timezone = "Asia/Hong_Kong",
         handler = false,
+        cacheFolder = false,
+        cacheTimer = 10000,
+        cacheLoopTimer = 1000,
+        cleanCacheLoop = true,
+        cache = false
     }) {
         var self = this;
         self._arguments = arguments[0]; //store arguments
@@ -38,6 +46,12 @@ class OdooApi {
         self._password = password;
         self._dateFormat = dateFormat;
         self._timezone = timezone;
+        self._cache = cache;
+        self._cacheTimer = cacheTimer;
+        self._cacheLoopTimer = 1000;
+        self._cacheFolder = cacheFolder;
+        self._cleanCacheLoop = cleanCacheLoop;
+        self._cacheMemory = {};
         self.exportMethodsToHandler(handler);
     }
 
@@ -60,8 +74,8 @@ class OdooApi {
         handler['insertModel'] = async function (model, item) {
             return self.insertModel(model, item);
         }
-        handler['getModelByFilters'] = async function (model = false, filters = [], fields = ['name'], offset = 0, limit = 100, sorting = false) {
-            return self.getModelByFilters(model, filters, fields, offset, limit, sorting);
+        handler['getModelByFilters'] = async function (model = false, filters = [], fields = ['name'], offset = 0, limit = 100, sorting = false, cached = true, cacheTimer = false) {
+            return self.getModelByFilters(model, filters, fields, offset, limit, sorting, cached, cacheTimer);
         }
 
         handler['executeWorkflow'] = async function (id, model, action, additional = []) {
@@ -70,6 +84,160 @@ class OdooApi {
         handler['actionModelByFilters'] = async function (action = false, model = false, filters = false, comparefields = false, validateCb = false, compareCb = false) {
             return self.actionModelByFilters(action, model, filters, comparefields, validateCb, compareCb);
         }
+    }
+
+    async getCache(key = false) {
+        var self = this;
+
+        if (!self._cache)
+            return false;
+
+        if (self._cacheFolder) {
+            //check for files with this key in this folder
+            var path = `${self._cacheFolder}/${key}.json`;
+            try {
+                var data = await self._helper.readJson(path);
+                self._logger.debug('Served Cache: ' + key);
+                return JSON.parse(data);
+            } catch (err) {
+                //console.log(err); //do not block
+                return false;
+            }
+        } else {
+            //cache is in memory, check array
+            return self._cacheMemory[key] ? self._cacheMemory[key] : false;
+        }
+        return false;
+    }
+
+    async getFileList(path = false) {
+        var self = this;
+        self._logger.func(`getFileList`);
+        if (!path)
+            return false;
+
+        return new Promise(res => {
+            readDirFiles.list(path, function (err, filenames) {
+                if (err) return console.dir(err);
+                res(filenames);
+            });
+        })
+    }
+
+    async cleanCacheLoop() {
+        var self = this;
+        if (!self._cache)
+            return false;
+        self._logger.func('cleanCacheLoop');
+
+        self._cacheLoop = setInterval(async function () {
+            self._logger.info('cleanCacheLoop: check');
+            if (self._cacheFolder) {
+                try {
+                    //get all files cached
+                    var files = await self.getFileList(self._cacheFolder);
+
+                    //chck for each if we need to remove
+                    for (var index = 0; index < files.length; index++) {
+                        var path = files[index]; //`${self._cacheFolder}/${key}.json`;
+                        if (self._helper.containsStr('.json', path)) {
+                            try {
+                                var data = await self._helper.readJson(path);
+                                data = JSON.parse(data);
+
+                                //must remove
+                                if (moment().diff(moment(data.date), 'ms') > data.expiration) {
+                                    await del.sync([path]);
+                                    //self._logger.debug('Removed Cache after ' + (data.expiration / 1000) + 's : ' + data.key);
+                                }
+                            } catch (err) {
+                                // console.log(err); //do not block
+                                //return false;
+                            }
+                        }
+                    }
+                    return true;
+                } catch (err) {
+                    //console.log(err) //do not block
+                    //self._logger.debug('Cannot remove cache: ' + key);
+                    return false;
+                }
+            } else {
+                Object.keys(self._cacheMemory).map(k => {
+                    try {
+                        if (moment().diff(moment(self._cacheMemory[k].date), 'ms') > self._cacheMemory[k].expiration) {
+                            // self._logger.debug('Removed Cache after ' + (self._cacheMemory[k].expiration / 1000) + 's : ' + self._cacheMemory[k].key);
+                            delete self._cacheMemory[k];
+                        }
+                    } catch (err) {
+                        // console.log(err); //do not block
+                    }
+                });
+                return true;
+            }
+        }, self._cacheLoopTimer)
+
+        return true;
+    }
+
+    async cleanAllCache() {
+        var self = this;
+        if (!self._cache)
+            return false;
+        self._logger.func('cleanAllCache');
+
+        if (self._cacheFolder) {
+            try {
+                //get all files cached
+                var files = await self.getFileList(self._cacheFolder);
+                for (var index = 0; index < files.length; index++) {
+                    var path = files[index]; //`${self._cacheFolder}/${key}.json`;
+                    if (self._helper.containsStr('.json', path)) {
+                        try {
+                            await del.sync([path]);
+                        } catch (err) {
+                            //  console.log(err); //do not block
+                            //return false;
+                        }
+                    }
+                }
+            } catch (err) {
+                //  console.log(err)
+            }
+        } else {
+            self._cacheMemory = {};
+        }
+
+        return true;
+    }
+
+    async setCache(key, data = false, expiration = false) {
+        var self = this;
+        if (!self._cache || !data)
+            return false;
+
+        var expiration = expiration ? expiration : self._cacheTimer;
+
+        // self._logger.func('setCache', key);
+        if (self._cacheFolder) {
+            //write in file
+            var path = `${self._cacheFolder}/${key}.json`;
+            await self._helper.writeJson(path, JSON.stringify({
+                date: moment().format(),
+                key: key,
+                expiration: expiration,
+                data: data
+            }));
+        } else {
+            //cache is in memory, check array
+            self._cacheMemory[key] = {
+                date: moment().format(),
+                key: key,
+                expiration: expiration,
+                data: data
+            };
+        }
+        return true;
     }
 
     gDateFormat() {
@@ -103,6 +271,11 @@ class OdooApi {
             maxRecordsPerCall: self._maxRecordsPerCall
         });
 
+        if (self._cleanCacheLoop) {
+            await self.cleanCacheLoop();
+            await self.cleanAllCache();
+        }
+
         await self._odooApi.initialize();
     }
 
@@ -110,7 +283,7 @@ class OdooApi {
     //UPDATE ANY MODEL IN DB DEPENDING ON USER RIGHTS
     async updateModel(model, item) {
         var self = this;
-       // self._logger.func('updateModel', model);
+        // self._logger.func('updateModel', model);
         return self._odooApi.updateModel(model, item);
     }
 
@@ -122,16 +295,35 @@ class OdooApi {
     }
 
     //GET ANY MODEL IN DB DEPENDING ON USER RIGHTS
-    async getModelByFilters(model = false, filters = [], fields = ['name'], offset = 0, limit = 100, sorting = false) {
+    async getModelByFilters(model = false, filters = [], fields = ['name'], offset = 0, limit = 100, sorting = false, cached = false, cacheTimer = false) {
         var self = this;
-       // self._logger.func(`getModelByFilters`, `${model}`); //${JSON.stringify(filters)}
-        return self._odooApi.getModelByFilters(model, filters, fields, offset, limit, sorting);
+        // self._logger.func(`getModelByFilters`, `${model}`); //${JSON.stringify(filters)}
+        if (self._cache && cached) {
+            var key = self._helper.md5(JSON.stringify({
+                model: model,
+                filters: filters,
+                fields: fields,
+                offset: offset,
+                limit: limit,
+                sorting: sorting
+            }));
+
+            var cache = await self.getCache(key);
+            if (cache)
+                return cache;
+
+            var data = await self._odooApi.getModelByFilters(model, filters, fields, offset, limit, sorting);
+            await self.setCache(key, data, cacheTimer);
+            return data;
+        } else {
+            return self._odooApi.getModelByFilters(model, filters, fields, offset, limit, sorting);
+        }
     }
 
     //Execute workflow
     async executeWorkflow(id, model, action, additional = []) {
         var self = this;
-       // self._logger.func('execute', `${action} on ${model}#${id}`);
+        // self._logger.func('execute', `${action} on ${model}#${id}`);
         return self._odooApi.executeWorkflow(id, model, action, additional);
     }
 
